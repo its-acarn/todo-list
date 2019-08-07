e2c328e52e2561dd9d9eb8502a29eaee5e26c52f
svvishjs <svvishjs@users.noreply.github.com>
2019-08-07 21:54:27 +0100

    To-do list is now up and working after updating javascript.

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
-console.log("it works");
+//add item to list
+function addTodoItem() {
+	let ul = document.getElementById("list");
+	let li = document.createElement("li");
+	let doneButton = document.createElement("button");
+	let deleteButton = document.createElement("button");
+	let listItem = [li,doneButton,deleteButton];
+	let input = document.getElementById("input").value;
 
-let myDone_Items = document.getElementsByClassName('done-items');
+	ul.appendChild(li);
+	li.appendChild(document.createTextNode(input));
+	li.appendChild(doneButton);
+	li.appendChild(deleteButton);
+	document.getElementById("input").value = "";
+	li.onclick = removeItem;
+};
+
+document.body.onkeyup = function(e) {
+	if (e.keyCode === 13) {
+	  addTodoItem();
+	}
+};
+
+document.getElementById("btn").onclick = addTodoItem;
+
+function removeItem(e) {
+	e.target.parentNode.removeChild(e.target);
+}
+
+//let button = document.getElementById("btn");
+//button.onclick = addTodoItem;
